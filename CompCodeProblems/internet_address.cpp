8804ad5581f5465813a262a2449d42506f9ecba8
Shanu Sharma <42693444+sharmashanu@users.noreply.github.com>
2020-08-14 11:55:55 +0530

    Update internet_address.cpp

--- CompCodeProblems/internet_address.cpp
+++ CompCodeProblems/internet_address.cpp
@@ -1,30 +1,41 @@
-int getRest(string str,int i)
-{
-	for(int j=i+1;j<str.size()-1;j++)
-	{
-		if(str[j]=='r'&&str[j+1]=='u')
-		return j-1-i;
-	}
- return -1;
-}
-void internetAddress(string s) {
-    string ptr =s;
-    int starter ,pos;
-    if(s[0]=='h'&&s[1]=='t'&&s[2]=='t'&&s[3]=='p')
-    {
-    	cout<<"http://";
-    	starter =3;
-    	pos = getRest(ptr,3);
-	}
-	else
-	{
-		cout<<"ftp://";
-		starter=2;
-		pos =getRest(ptr,2);
-	}
-	cout<<(ptr.substr(starter+1,pos));
-	cout<<".ru";
-	pos=starter+pos+2;
-	cout<<"/";
-	cout<<ptr.substr(pos+1);
-}
+#include<iostream>
+        #include<string>
+        using namespace std;
+        int getPos(string str,int st){						//Returns the length of string after 'st' and before 'ru'
+            int i=0;
+           if(str[st]=='r' and str[st+1]=='u') i+=2;
+            for(i+=st;str[i];i++){
+                if(str[i]=='r' and str[i+1]=='u')
+                    return i-st;
+            }
+            return -1;
+        }
+         
+        void internetAddress(string s) {
+            int len=0,i,st;
+            if(s[0]=='h' and s[1]=='t' and s[2]=='t' and s[3]=='p'){
+                len=getPos(s,4);
+                st=4;
+                cout<<"http://";
+            }
+            else 
+            {
+                if(s[0]=='f' and s[1]=='t' and s[2]=='p'){
+                    len=getPos(s,3);
+                    st=3;
+                    cout<<"ftp://";
+                }
+            }
+            cout<<s.substr(st,len);
+            cout<<".ru";
+            if(st+len+2==s.size()) return;
+            cout<<"/";
+            cout<<s.substr(st+len+2,s.size()-(st+len+2));
+            return;
+        }
+        int main(){
+            string st;
+            cin>>st;
+            internetAddress(st);
+            return 0;
+        }
